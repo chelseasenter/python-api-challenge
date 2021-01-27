da22c655ea45b2ac9b3c036ca71742dda9ded352
Chelsea Senter <chelsea.senter10@gmail.com>
2021-01-26 19:38:11 -0500

    deleted API keys (woops)

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,6 +1,6 @@
 #OpenWeatherMap API key
-weather_api_key = 'ae9e92d4be0de207dde586d18e3e1ce4'
+weather_api_key = 'YOUR API KEY HERE'
 
 
 #Google Places API key
-google_api_key = 'AIzaSyD-IyIegJK11raenizBDtf1FOXeg-N3XCs'
+google_api_key = 'YOUR API KEY HERE'
